a86506c97181b1128976f11c23e5e9a2eb1c878e
Chris <christopherduggan@gmail.com>
2023-07-21 06:23:01 -0400

    db_url mod

--- api/db/connection.js
+++ api/db/connection.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose'
 
-const connectionString = process.env.DB_URL || 'mongodb://localhost:27017/countries'
+const connectionString = process.env.DB_URL
 
 let mongooseConnectionConfig = { useNewUrlParser: true, useUnifiedTopology: true }
 
