8f8caa58f5feee581fac3a939c1a3980c3981744
Chris <christopherduggan@gmail.com>
2023-07-21 05:59:26 -0400

    configure for deployment

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,10 @@
 import mongoose from 'mongoose'
 
+const connectionString = process.env.DB_URL || 'mongodb://localhost:27017/countries'
+
 let mongooseConnectionConfig = { useNewUrlParser: true, useUnifiedTopology: true }
-mongoose.connect('mongodb://localhost:27017/countries', mongooseConnectionConfig)
+
+mongoose.connect(connectionString, mongooseConnectionConfig)
 
 mongoose.connection.on('connected', ()=> console.log("Connected to database"))
 mongoose.connection.on('disconnected', ()=> console.log("Disconnected from database"))
--- index.js
+++ index.js
@@ -5,10 +5,11 @@ import Country from './models/Country.js'
 const app = express()
 app.use(express.json())
 
+const PORT = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
     Country.find({})
         .then(country => res.json(country))
 })
 
-app.listen(3000, () => console.log('listening on 3000'))
+app.listen(PORT, () => console.log('listening on 3000'))
