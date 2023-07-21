b1f67404833d13c27f733b9a76829ee4c64f7bed
Chris <christopherduggan@gmail.com>
2023-07-21 07:52:47 -0400

    add experimental

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose'
 
-const connectionString = process.env.DB_URL || 3000
+const connectionString = process.env.DB_URL || 'mongodb://localhost/countries'
 
 let mongooseConnectionConfig = { useNewUrlParser: true, useUnifiedTopology: true }
 
